844b629997a65e1b217007be94c9eb32144fef10
MrRumpel <hades137747575@gmail.com>
2023-05-29 11:10:07 +0800

    1 file modified, 3 added

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: false,
+  // distDir: "./public/next",
+  // basePath: "/home", //node
+  typescript: {
+    tsconfigPath: './tsconfig.next.json',
+  },
+};
